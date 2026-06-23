7945ae1cb8e6347381bcbc293f2ca11ebd3c04c7
fariosofernando <ff.m@tutamail.com>
2026-06-23 13:19:57 +0200

    chore: *

--- lib/infra/interfaces/routes/route_handler.ts
+++ lib/infra/interfaces/routes/route_handler.ts
@@ -1,24 +1,24 @@
 import { Router } from "express";
-import Controller from "../controllers/controller.ts";
+import type Controller from "../controllers/controller.ts";
 
 export default class RouterHandler {
-  public handler;
+	public handler;
 
-  private controller: Controller;
+	private controller: Controller;
 
-  constructor(ctr: Controller) {
-    this.handler = Router();
+	constructor(ctr: Controller) {
+		this.handler = Router();
 
-    this.controller = ctr;
-    this.bind_routes();
-  }
+		this.controller = ctr;
+		this.bind_routes();
+	}
 
-  private bind_routes() {
-    this.handler.post("/bucket", this.controller.create_bucket);
-    this.handler.post("/file/upload", this.controller.upload);
-    this.handler.get("/files/:bucket_id", this.controller.list_dir);
+	private bind_routes() {
+		this.handler.post("/bucket", this.controller.create_bucket);
+		this.handler.post("/file/upload", this.controller.upload);
+		this.handler.get("/files/:bucket_id", this.controller.list_dir);
 
-    this.handler.get("/file/:bucket_id", this.controller.download_file);
-    this.handler.head("/file/:bucket_id", this.controller.file_data);
-  }
+		this.handler.get("/file/:bucket_id", this.controller.download_file);
+		this.handler.head("/file/:bucket_id", this.controller.file_data);
+	}
 }
--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,10 @@
-import express from "express";
 import cors from "cors";
-import auth from "./infra/middleware/auth";
+import express from "express";
 import file_upload from "express-fileupload";
-import RouterHandler from "./infra/interfaces/routes/route_handler";
 import Controller from "./infra/interfaces/controllers/controller";
+import RouterHandler from "./infra/interfaces/routes/route_handler";
+import auth from "./infra/middleware/auth";
+
 const v1 = "/v1/api/2bucket";
 
 const env = process.env.SERVICE_ENV;
@@ -13,27 +14,26 @@ const app = express();
 app.use(express.json());
 app.set("trust proxy", true);
 app.use(
-  // set your origin confs
-  cors({
-    origin: "*",
-  }),
+	cors({
+		origin: "*",
+	}),
 );
 
 app.get("/health", (_req, res) => res.status(200).json({ status: "ok" }));
 const router = new RouterHandler(new Controller());
 app.use(express.json());
 app.use(
-  file_upload({
-    limits: { fileSize: 10 * 1024 * 1024 },
-    useTempFiles: true,
-    tempFileDir: "/app/files",
-  }),
+	file_upload({
+		limits: { fileSize: 10 * 1024 * 1024 },
+		useTempFiles: true,
+		tempFileDir: "/app/files",
+	}),
 );
 
 app.use(v1, auth, router.handler);
 
 app.listen(4000, "0.0.0.0", () => {
-  console.info(
-    `Service running on port ${PORT}, in ${env === "prod" ? "production" : "development"} environment.`,
-  );
+	console.info(
+		`Service running on port ${PORT}, in ${env === "prod" ? "production" : "development"} environment.`,
+	);
 });
